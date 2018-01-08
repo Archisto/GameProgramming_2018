55563bf7db7e991e02335502d03654051247832b
Lauri Kosonen <lauri.kosonen@cs.tamk.fi>
2018-01-08 11:44:46 +0200

    Created a tank unit

---
+++ TankGame/Assets/Scripts/PlayerUnit.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace TankGame
+{
+    public class PlayerUnit : MonoBehaviour
+    {
+        public PlayerUnit()
+        {
+
+        }
+    }
+}
---
+++ TankGame/Assets/Scripts/Unit.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace TankGame
+{
+    public abstract class Unit : MonoBehaviour
+    {
+        public virtual void Init()
+        {
+
+        }
+
+        public virtual void Clear()
+        {
+
+        }
+
+        /// <summary>
+        /// Update is called once per frame.
+        /// An abstract method has to be defined
+        /// in a non-abstract child class.
+        /// </summary>
+        protected abstract void Update();
+    }
+}
